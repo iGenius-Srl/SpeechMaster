b5db1eda1470612e6d80c51c9c6c0897a7f47186
Kristiyan Petrov <petkris_bg@hotmail.it>
2017-10-05 16:16:13 +0200

    create lib

---
+++ Example/Pods/Target Support Files/Pods-SpeechMaster_Example/Pods-SpeechMaster_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SpeechMaster_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SpeechMaster_Example
+@end
---
+++ Example/Pods/Target Support Files/SpeechMaster/SpeechMaster-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SpeechMaster : NSObject
+@end
+@implementation PodsDummy_SpeechMaster
+@end
